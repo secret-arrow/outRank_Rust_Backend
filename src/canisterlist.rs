f90a2cdd1750ecae7cf752f6a2f5bf38cdefa5f7
crazycrab0x2 <maxbranch1110@gmail.com>
2023-08-31 17:05:10 -0400

    fetch data

---
+++ src/canisterlist.rs
@@ -0,0 +1,25 @@
+pub fn get_canister_list() -> Vec<String> {
+    let canisterlist = vec![
+        String::from("Xizxk-fqaaa-aaaap-aa2nq-cai"),
+        String::from("vsf7w-3aaaa-aaaap-aa2cq-cai"),
+        String::from("cr2rc-vaaaa-aaaap-aakwa-cai"),
+        String::from("Ktlvk-giaaa-aaaap-aayja-cai"),
+        String::from("n5yqx-uqaaa-aaaap-aatja-cai"),
+        String::from("qv3zw-kqaaa-aaaap-aa27q-cai"),
+        String::from("ah2fs-fqaaa-aaaap-aalya-cai"),
+        String::from("nwyv2-gaaaa-aaaaj-arc2a-cai"),
+        String::from("qqmo2-iyaaa-aaaap-aajwq-cai"),
+        String::from("355dh-iiaaa-aaaap-aa46q-cai"),
+        String::from("q6ods-tiaaa-aaaap-aajxq-cai"),
+        String::from("kafas-uaaaa-aaaao-aaofq-cai"),
+        String::from("jx6g3-miaaa-aaaap-aamwa-cai"),
+        String::from("xpegl-kaaaa-aaaah-abcrq-cai"),
+        String::from("gw7hg-iqaaa-aaaap-aanqq-cai"),
+        String::from("bjcsj-rqaaa-aaaah-qcxqq-cai"),
+        String::from("fioai-paaaa-aaaap-aanzq-cai"),
+        String::from("u44c2-mqaaa-aaaap-aa5ya-cai"),
+        String::from("fnzxe-niaaa-aaaap-aa6qq-cai"),
+        String::from("slzze-ciaaa-aaaah-aa7ra-cai")
+    ];
+    canisterlist
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+mod canisterlist;
+
+use std::process::Command;
+use serde_json::to_string_pretty;
+
+
+#[tokio::main]
+async fn main() {
+    let canister_list = canisterlist::get_canister_list();
+
+    let mut trait_list:Vec<String> = Vec::new();
+
+    for canister in canister_list{
+        let output = Command::new("dfx")
+            .arg("canister")
+            .arg("call")
+            .arg("--network")
+            .arg("ic")
+            .arg(canister)
+            .arg("getTokens")
+            .output()
+            .expect("failed to deploy websites");
+
+        if output.status.success() {
+            let stdout = String::from_utf8_lossy(&output.stdout).to_string();
+            let pretty_string = to_string_pretty(&stdout).expect("can't convert string");
+            trait_list.push(pretty_string);
+            // println!("{}", stdout);
+        }else {
+            // let stderr = String::from_utf8_lossy(&output.stderr);
+            // println!("Command failed: {}", stderr);
+            trait_list.push(String::from("None"));
+        }
+    }
+}
+
+
+//trait normalization comparizon
+async fn trait_normalization(nft_collections_trait: String) -> Vec<u32>{
+    //normalize each collections traits
+}
+
+//QR codes
+async fn qrcode_generate(nft_collections_trait: String) -> Vec<u32>{
+    //generate qrcode
+}
+
+//Barcodes
+async fn barcode_generate(nft_collections_trait: String) -> Vec<u32>{
+    //generate barcode
 }
